4eccec2fd0d1fe9f670d402dd6741e8fc22d3827
devansh <devanshkg19@gmail.com>
2026-07-13 22:46:28 +0530

    refactor: rewrite vite plugin to support persistent css file generation and build-time bundling

--- packages/vite-plugin-morphcss/src/options.ts
+++ packages/vite-plugin-morphcss/src/options.ts
@@ -1,15 +1,9 @@
 export interface MorphCSSOptions {
   utilities?: string | string[];
   output?: string;
-  emitFile?: boolean;
-  dev?: boolean;
-  hmr?: boolean;
 }
 
 export const defaultOptions: Required<MorphCSSOptions> = {
   utilities: [],
-  output: "morph.css",
-  emitFile: true,
-  dev: false,
-  hmr: true,
+  output: ".morphcss/morph.css",
 };
