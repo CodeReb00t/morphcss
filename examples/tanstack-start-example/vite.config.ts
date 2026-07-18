caea209bd4d7e7cd9db9f10035b53ba299b537f7
devansh <devanshkg19@gmail.com>
2026-07-18 13:11:02 +0530

    chore: add nitro for vercel tanstack start deployment

--- examples/tanstack-start-example/vite.config.ts
+++ examples/tanstack-start-example/vite.config.ts
@@ -2,13 +2,14 @@ import { devtools } from "@tanstack/devtools-vite";
 import { defineConfig } from "vite";
 
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 import { morphcss } from "@morph-css/kit/vite";
 import viteReact from "@vitejs/plugin-react";
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), morphcss(), tanstackStart(), viteReact()],
+  plugins: [devtools(), morphcss(), tanstackStart(), nitro(), viteReact()],
 });
 
 export default config;
